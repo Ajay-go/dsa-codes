ce9bfd3a1d7e56ad175770032bd4d170913ea275
ajay gour <ajaygour3333@gmail.com>
2025-07-25 21:12:01 +0530

    added morris order traversal

---
+++ morris_traversal.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class TreeNode
+{
+    TreeNode* left = NULL;
+    TreeNode *right = NULL;
+    int val;
+
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+
+        auto curr = root;
+        while (curr)
+        {
+            if (!curr->left)
+            {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else
+            {
+                auto leftnode = curr->left;
+                while (leftnode && leftnode->right)
+                {
+                    leftnode = leftnode->right;
+                }
+                leftnode->right = curr;
+
+                auto c = curr;
+                curr = curr->left;
+                c->left = NULL;
+            }
+        }
+        return ans;
+    }
+};
+int main() {
+
+    
+    return 0;
+}
