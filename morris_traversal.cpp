7b18bcd25e84c1a7816d97876a05ca4094dfecf4
ajay gour <ajaygour3333@gmail.com>
2025-07-25 21:30:39 +0530

    added morris preorder

--- morris_traversal.cpp
+++ morris_traversal.cpp
@@ -39,6 +39,35 @@ public:
         }
         return ans;
     }
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        auto curr = root;
+        vector<int> ans;
+        while (curr)
+        {
+            if (!curr->left)
+            {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else
+            {
+                auto leftnode = curr->left;
+                while (leftnode->right)
+                    leftnode = leftnode->right;
+                if (curr->right)
+                    leftnode->right = curr->right;
+                else
+                    leftnode->right = NULL;
+
+                auto c = curr;
+                ans.push_back(c->val);
+                curr = curr->left;
+                c->left = NULL;
+            }
+        }
+        return ans;
+    }
 };
 int main() {
 
