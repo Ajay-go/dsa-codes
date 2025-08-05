5ab94cf5731041e39d05ed2576e952d9eac5462c
ajay gour <ajaygour3333@gmail.com>
2025-08-05 21:37:56 +0530

    some changes

--- abstract_classes.cpp
+++ abstract_classes.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <exception>
 
 using namespace std;
-class A{
+
+class A
+{
 public:
-    virtual void add(int a,int b)=0;//purely virutal function then this is abstract class 
+    virtual void add(int a, int b) = 0; // Pure virtual function
 };
-class B:public A{
-    public:
-    void add(int a,int b) override {
-        cout<<a+b;
+
+class B : public A
+{
+public:
+    void add(int a, int b) override
+    {
+        cout << a + b << endl;
     }
 };
-int main() {
-    //A obj1  this will give error as we cannot make object of abstract classes(A)
-    B obj;
-    obj.add(10,5);
-    int x = 10;
-    int *ad;
-   
-    ad = &x;
-    cout<<ad;
-    cout<<endl;
-    cout<<*ad;
 
-    
+int main()
+{
+    try
+    {
+        //A obj1; // Uncommenting this will cause a compile-time error
+        throw runtime_error("Cannot create object of abstract class A");
+    }
+    catch (const exception &e)
+    {
+        cout << e.what() << endl;
+    }
+
+    B obj;
+    obj.add(10, 5);
+
+    int x = 10;
+    int *ad = &x;
+
+    cout << "Address: " << ad << endl;
+    cout << "Value: " << *ad << endl;
+
     return 0;
 }
