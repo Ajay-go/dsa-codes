28f3b6ee7d07dac9cadf3252c76a222125f39be5
ajay gour <ajaygour3333@gmail.com>
2025-08-17 22:07:53 +0530

    insertion sort added

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -9,10 +9,17 @@ int main() {
     vector<int>v = {-1,0,2,7,-5};
     int n = v.size();
 
-    for(int i =0;i<n;i++){
-        for(int j = i;j>=0;j--){
-            
+    for(int i =1;i<n;i++){
+        int curr = v[i];
+
+        int j = i-1;
+        while(j >= 0 && v[j] > curr){
+            v[j+1] = v[j];
+            j--;
         }
+        v[j+1] = curr;
+        
     }
+    for(int ele:v)cout<<ele<<" ";
     return 0;
 }
